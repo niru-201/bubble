01e85247b34761e8abe8165d374e3e845456a6f1
niru-201 <63295375+niru-201@users.noreply.github.com>
2020-05-15 20:05:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ var restart = document.getElementById("restart");
 var play = document.getElementById("play");
 var bubble = [];
 var num =2;
-var circ;
-var rate = 2000;
 var touch=0;
+var circ;
+var rate =2000;
 var timer=0;
-var m=0,s=0,ms=0;
+var min=0,sec=0,ms=0;
 var score=0;
 var hs = localStorage.getItem("hs");
 var status =0,gstat=0;
 var gauntr=60000;
-var addbinterval,drawinterval;
+var addbubbleinterval,drawinterval;
 var colors = ["rgba(106,73,255,0.4)","rgba(241,238,22,0.4)","rgba(255,71,157,0.4)","rgba(47,243,63,0.4)","rgba(47,243,63,0.5)"];
 var bgmusic= new Audio("bgmusic.mp3");
 var pops= new Audio("pops.mp3");
@@ -33,10 +33,12 @@ dist = (x1,y1,x2,y2) => {
   var b = y1-y2;
     return((Math.sqrt(a**2 +b**2)));
 } 
+//Intial page
 setup = () => {  
    body.removeChild(layout);
 }
 setup();
+//Area calculation
 area = () => {
   var areac=0;
   for(var i=0;i<bubble.length;i++)
@@ -63,15 +65,15 @@ const playPromise2 =bgmusic.play();
 }
 run = () =>
 { ratec();
-  timerclock.textContent="TIME:"+(m<10? "0"+m : m)+":"+(s<10? "0"+s: s)+":"+(ms<10?"0"+ms:ms) + "  BEST:"+localStorage.getItem("hs");
+  timerclock.textContent="TIME:"+(min<10? "0"+min : min)+":"+(sec<10? "0"+sec: sec)+":"+(ms<10?"0"+ms:ms);
   ms++;
   if(ms==100)
   {ms=0;
-  s++;
+  sec++;
   }
-  if(s==60)
-  {s=0;
-  m++;
+  if(sec==60)
+  {sec=0;
+  min++;
   }
 } 
  stop = () =>
@@ -87,9 +89,9 @@ run = () =>
 }
   timer=0;
  ms=0;
- s=0;
- m=0;
- timerclock.textContent=(m<10? "0"+m : m)+":"+(s<10? "0"+s: s)+":"+(ms<10?"0"+ms:ms)+ "  BEST:"+localStorage.getItem("hs");
+ sec=0;
+ min=0;
+ timerclock.textContent=(min<10? "0"+min: min)+":"+(sec<10? "0"+sec: sec)+":"+(ms<10?"0"+ms:ms);
 }
  reset = () =>
 {stop();
@@ -98,18 +100,19 @@ document.location.reload();
 ratec = () => {
   if(score>75)
      gauntr=30000;
-  if(m<1)
+  if(min<1)
     rate =2000;
-  else if(m<2)
+  else if(min<2)
     { num=4;
     rate = 1000;}
-  else if(m<3)
+  else if(min<3)
     {rate = 800; 
       num=6;}
-  else if(m<4)
+  else if(min<4)
     {rate = 700;  
       num=7;}   
 }
+//Gauntlets
 const gaunt = {
   draw : function(){
   ctx.drawImage(sprite,317,298,217,201,650,5,80,80);
@@ -125,12 +128,13 @@ gauntact = (x,y) =>{
 gaunt.draw(); 
 gaunt.burst(x,y);
 }
-
+//To invoke gauntlets
 gauntadd =() =>{
   gstat =1;
 }
 
 setInterval(gauntadd,gauntr);
+//Game over animation
 gameover = () =>{
   ctx.clearRect(0,0,cvs.width,cvs.height);
   ctx.font = "50px Arial";
@@ -138,6 +142,7 @@ gameover = () =>{
   ctx.fillText("GAME OVER",265,200);
   ctx.fillText("SCORE: "+JSON.stringify(score),285,260);
 }
+//Play button function
 play.addEventListener("click",function(){
     body.removeChild(frame);
     body.appendChild(layout);
@@ -147,19 +152,21 @@ play.addEventListener("click",function(){
 restart.addEventListener("click",function(){
  reset();
 })
+//pause button
 pause.addEventListener("click",function(){
     if(pause.textContent== "PAUSE")
      { pause.textContent = "RESUME";
       clearInterval(timer);
       clearInterval(drawinterval);
-      clearInterval(addbinterval);}
+      clearInterval(bubble_add);}
     else if(pause.textContent == "RESUME"){
         pause.textContent = "PAUSE";
      timer= setInterval(run,10);
      drawinterval =setInterval(draw,50);
-     addbinterval= setInterval(addb,rate);
+     bubble_add= setInterval(addb,rate);
     }  
 })
+//checking gameover condition
 over = () => {
   if(area()){
     window.setTimeout(area(),10000);
@@ -168,25 +175,26 @@ over = () => {
       best();
       clearInterval(timer);
       clearInterval(drawinterval);
-      clearInterval(addbinterval);
+      clearInterval(bubble_add);
       window.setTimeout(reset,7000);
     }
   }
 }
+//bubble properties
 class circle{
   constructor(){
     this.x = Math.random()*740;
     this.y = Math.random()*540;
     this.rad = Math.random()*20 + 30;
-    this.dx =2;
-    this.dy = -2;
+    this.dx =  0.7;
+    this.dy = -0.7;
     this.vel = Math.random()/5;
   }
   }
 newbubble = () => {
   circ = new circle();
    bubble.push(circ);
-}  
+} 
 drawbubble = (i) => {
   
   ctx.beginPath();
@@ -195,13 +203,15 @@ drawbubble = (i) => {
   ctx.fill();
 
 }  
+//intial bubbles
 pusharr = () => {
- { for(var i=0;i<15;i++)
+ { for(var i=0;i<10;i++)
       newbubble();
   } 
  for(var i=0;i<bubble.length;i++) 
     drawbubble(i);
 }
+//popping bubbles
 popbbl = (a,b) => {
   for(var i=0;i<bubble.length;i++){
     if((dist(a,b,bubble[i].x,bubble[i].y)) < bubble[i].rad)
@@ -227,19 +237,21 @@ popbbl = (a,b) => {
         gauntt= setInterval(gauntact(xpos,ypos),40);
       }
  });
+ //highscore calc.
 best= () => {
   if(hs != null){
     if(parseInt(hs) < score)
       localStorage.setItem("hs",JSON.stringify(score));}
   else
    localStorage.setItem("hs",JSON.stringify(score));}
-
-addb = () => {
+//generating bubbles
+addbubble = () => {
   for(var i=0;i<num;i++)
    { newbubble();
     drawbubble(bubble.length-1);}
   over();
 }
+//liquid luck
 liqluck = () => {
 ctx.drawImage(sprite,125,119,197,188,5,5,80,80);}
 liq = (mx,my) =>{
@@ -248,29 +260,46 @@ liq = (mx,my) =>{
     status++;
     pops.currentTime=0;
     pops.play();
-         clearInterval(addbinterval);
-         setTimeout(function(){setInterval(addb,rate)},10000);
+         clearInterval(bubble_add);
+         setTimeout(function(){setInterval(bubble_add)},10000);
     if(status ==2)   
         clearInterval(liqt);
     }
 
 }
+//Main drawing function
 draw = () => {
   ctx.clearRect(0,0,cvs.width,cvs.height);
   if(gstat==1)
      gaunt.draw();
+     ctx.font = "30px Arial";
+     ctx.fillStyle = "white";
+     ctx.fillText("BEST:" + localStorage.getItem("hs"),600,70);
+     ctx.fillText("SCORE:"+JSON.stringify(score),600,32);   
   for(var i=0;i<bubble.length;i++){
-    drawbubble(i); 
+   for(var j=i+1;j<bubble.length;j++){
+     if(dist(bubble[i].x,bubble[i].y,bubble[j].x,bubble[j].y)<=bubble[i].rad+bubble[j].rad){
+       var angle = Math.atan2(bubble[j].y-bubble[i].y,bubble[j].x-bubble[i].x);
+       tx= bubble[i].x + Math.cos(angle)*(bubble[i].rad+bubble[j].rad);
+       ty = bubble[i].y + Math.sin(angle)*(bubble[i].rad+bubble[j].rad);
+       ax = (tx - bubble[j].x)*0.015;
+       ay = (ty - bubble[j].y)*0.015;
 
+       bubble[i].dx -=ax;
+       bubble[i].dy -=ay;
+       bubble[j].dx +=ax;
+       bubble[j].dy +=ay;
+     }
+   }
 if(bubble[i].y + bubble[i].dy > (cvs.height-bubble[i].rad) || (bubble[i].y + bubble[i].dy) < bubble[i].rad) 
     bubble[i].dy = -bubble[i].dy;
 if(bubble[i].x + bubble[i].dx > (cvs.width-bubble[i].rad) || (bubble[i].x + bubble[i].dx) < bubble[i].rad)
     bubble[i].dx = -bubble[i].dx;
 bubble[i].x += bubble[i].dx;
 bubble[i].y += bubble[i].dy; 
- 
+drawbubble(i); 
 }
 }
-drawinterval = setInterval(draw,40);
-addbinterval = setInterval(addb,rate);
+drawinterval = setInterval(draw,30);
+bubble_add = setInterval(addbubble,rate);
 liqt = setInterval(liqluck,40);
